11285c2f45feff59b995bee80d1bb70d585a580a
Quang Nguyen <wan15112001@gmail.com>
2021-01-01 11:27:48 -0800

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,6 @@
+# update npm
+sudo npm install --global npm@latest
+# update my npm packages
+sudo npm install --global expo-cli sharp-cli firebase-tools nodemon typescript mocha pnpm
+# update brew packages
+brew update
